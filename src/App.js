0be132fc5681cba8aa113c8f4a5dbd680cafe052
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2022-06-28 18:22:35 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
+//change game to start running when the user starts typing if he doesnt do that in the course
+//change
 function App() {
+
+const [pressedChars, setPressedChars] = useState({
+  word: ''
+})
+const [wordCount, setWordCount] = useState(0)
+const [timeRemaining, setTimeRemaining] = useState(10)
+const [gameStart, setGameStart] = useState(false)
+
+const handleChange = (event) => {
+  setPressedChars(prev => ({
+...prev,
+word: event.target.value
+  }))
+}
+
+const handleWordCount = (text) => {
+ setTimeRemaining(10)
+ setWordCount(0)
+ setPressedChars({
+   word: ''
+ })
+  setGameStart(true)
+} 
+
+const displayWordsCounted = () => {
+  let wordsCounted = pressedChars.word
+  .trim()
+  .split(' ')
+  .filter(words => words !== '')
+  setWordCount(wordsCounted.length)
+}
+
+useEffect(()=>{
+  if(timeRemaining > 0 && gameStart){
+  setTimeout(()=>
+  {
+setTimeRemaining(timeRemaining - 1)
+  }, 1000)
+}else if(timeRemaining === 0) {
+  setGameStart(false)
+  displayWordsCounted()
+}
+}, [timeRemaining, gameStart])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>Do You Type Fast?</h1>
+<form><textarea
+type='text'
+value={pressedChars.word}
+name='type here'
+placeholder='Begin typing here'
+onChange={handleChange}
+disabled={!gameStart}
+
+/>
+</form>
+<h4>Time Remaining: {timeRemaining} </h4>
+<button onClick={()=> handleWordCount()} disabled={gameStart}>Start Game</button>
+<h1>Word Count: {wordCount} </h1>
     </div>
   );
 }
